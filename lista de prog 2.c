c5adc19f1baa7fe9b967b38d6271134c4d002d5b
Eduardo Reis <102276374+lKyohei@users.noreply.github.com>
2022-03-30 15:02:38 -0300

    Add files via upload

---
+++ lista de prog 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+	
+	int idade;
+	char nome;
+	
+	printf("Digite seu nome:");
+	scanf("%s", &nome);
+	printf("Digite sua idade:");
+	scanf("%i", &idade);
+	
+	
+}
